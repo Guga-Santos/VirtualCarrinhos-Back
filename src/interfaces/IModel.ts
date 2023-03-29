2a5f34babc79f499ed3dd6a8dc93ae7956e41746
GugaSantos <guga.santos.guga@gmail.com>
2023-03-29 08:42:55 -0300

    fix: update params

--- src/interfaces/IModel.ts
+++ src/interfaces/IModel.ts
@@ -2,6 +2,6 @@ export interface IModel<T> {
   create(obj: T): Promise<T>,
   read(): Promise<T[] | null>,
   readOne(_id: string): Promise<T | null>,
-  update(_id: string): Promise<T | null>,
+  update(_id:string, obj:Partial<T>): Promise<T | null>,
   delete(_id: string): Promise<T | null>
 }
