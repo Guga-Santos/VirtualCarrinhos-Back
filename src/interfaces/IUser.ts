1414b7e5cf2b85077db7d9d74daff8df1f6dc95b
GugaSantos <guga.santos.guga@gmail.com>
2023-03-29 07:00:04 -0300

    fix: add role at user inferface

--- src/interfaces/IUser.ts
+++ src/interfaces/IUser.ts
@@ -6,6 +6,7 @@ const UserZodSchema = z.object({
   name: z.string().min(3),
   email: z.string().email(),
   password: z.string().min(6),
+  role: z.string(),
 });
 
 export type IUser = z.infer<typeof UserZodSchema>;
