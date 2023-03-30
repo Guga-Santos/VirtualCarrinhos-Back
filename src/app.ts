4966ed8c7b169c107dd57df7660dbca845d7a64d
GugaSantos <guga.santos.guga@gmail.com>
2023-03-30 09:33:20 -0300

    feat: app.use json()

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import 'express-async-errors';
 import errorHandler from './middlewares/error';
+import carRouter from './routes/Car';
 
 const app = express();
+app.use(express.json());
+app.use(carRouter);
 app.use(errorHandler);
 
 export default app;
