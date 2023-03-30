fde79283adf2c86618f32c1f1e39c6b435d89747
GugaSantos <guga.santos.guga@gmail.com>
2023-03-30 07:14:00 -0300

    feat: delete and update methods at cars controller

--- src/controllers/Cars.ts
+++ src/controllers/Cars.ts
@@ -23,4 +23,20 @@ export default class CarsController {
 
     return res.status(200).json(car);
   }
+
+  public async update(req: Request, res: Response) {
+    const { id } = req.params;
+    const { body } = req;
+
+    const updated = await this._service.update(id, body);
+
+    return res.status(201).json(updated);
+  }
+
+  public async delete(req: Request, res: Response) {
+    const { id } = req.params;
+
+    await this._service.delete(id);
+    return res.status(204).end('Successfully deleted');
+  }
 }
