ff92c11a0a6c9c3b2e723bd43fa177d6a7ce5456
GugaSantos <guga.santos.guga@gmail.com>
2023-03-31 16:09:54 -0300

    test: create method at acessories service

---
+++ src/tests/unit/services/acessories.service.test.ts
@@ -0,0 +1,42 @@
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import { ZodError } from 'zod';
+
+import Acessories from "../../../models/AcessoriesModel";
+import AcessoriesService from "../../../service/Acessories";
+import { acessoryMock, acessoryMockWithId } from '../../mocks/acessoryMocks';
+
+describe('Acessories Service Suite Tests', () => {
+  const acessoryModel = new Acessories();
+  const acessoryService = new AcessoriesService(acessoryModel);
+
+  before(() => {
+    sinon.stub(acessoryModel, 'create').resolves(acessoryMockWithId);
+    
+    sinon.stub(acessoryMock, 'read')
+      .onCall(0).resolves([acessoryMockWithId])
+      .onCall(1).resolves(null);
+  })
+  
+  after(() => {
+    sinon.restore();
+  })
+
+  describe('Create Acessories', () => {
+    it('On success', async () => {
+      const newAcessory = await acessoryService.create(acessoryMock)
+
+      expect(newAcessory).to.be.deep.equal(acessoryMockWithId);
+    })
+
+    it('On failure', async () => {
+      let error;
+      try {
+        await acessoryService.create({});
+      } catch(err) {
+        error = err
+      }
+      expect(error).to.be.instanceOf(ZodError);
+    })
+  })
+})
