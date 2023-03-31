1e5e9611d1ffef397f8b7a98af4c5262df943d0b
GugaSantos <guga.santos.guga@gmail.com>
2023-03-31 17:11:08 -0300

    test: read method at user service

--- src/tests/unit/services/users.service.test.ts
+++ src/tests/unit/services/users.service.test.ts
@@ -13,6 +13,10 @@ describe('Users Service Suite Tests', () => {
 
   before(() => {
     sinon.stub(userModel, 'create').resolves(userMockWithId);
+
+    sinon.stub(userModel, 'read')
+    .onCall(0).resolves([userMockWithId])
+    .onCall(1).resolves(null);
   })
 
   after(() => {
@@ -34,6 +38,18 @@ describe('Users Service Suite Tests', () => {
       }
 
       expect(error).to.be.instanceOf(ZodError);
+    })
+  })
+
+  describe('Read All Users', () => {
+    it('On success', async() => {
+      const list = await userService.read();
+      expect(list).to.be.deep.equal([userMockWithId]);
+    })
+
+    it('On failure', async() => {
+      const list = await userService.read();
+      expect(list).to.be.deep.equal(null);
     })
   })
 })
