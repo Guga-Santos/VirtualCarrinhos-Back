532f86836d43a174da8110505a115e8face8efa0
GugaSantos <guga.santos.guga@gmail.com>
2023-04-01 06:54:57 -0300

    test: create method at user controller

--- src/tests/unit/controllers/users.controller.test.ts
+++ src/tests/unit/controllers/users.controller.test.ts
@@ -2,12 +2,38 @@ import UsersController from "../../../controllers/Users";
 import Users from "../../../models/UserModel";
 import UsersService from "../../../service/Users";
 
+import { expect } from "chai";
+import { Request, Response } from "express";
+import * as sinon from 'sinon';
+import { userMock } from "../../mocks/userMocks";
+
 describe('Users Controller Suite Tests', () => {
   const userModel = new Users();
   const userService = new UsersService(userModel);
   const userController = new UsersController(userService);
 
-  before(() => {});
+  const req = {} as Request;
+  const res = {} as Response;
 
-  after(() => {});
+  before(() => {
+    sinon.stub(userService, 'create').resolves(userMock);
+
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns(res);
+    res.end = sinon.stub().returns(res);
+  });
+
+  after(() => {
+    sinon.restore();
+  });
+
+  describe('Create a new User', () => {
+    it('On success', async () => {
+      req.body = userMock;
+      await userController.create(req, res);
+
+      expect((res.status as sinon.SinonStub).calledWith(201)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(userMock)).to.be.true;
+    })
+  })
 })
