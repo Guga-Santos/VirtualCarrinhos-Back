44fb325c782e7fa78cbba26f336b64cde7e5101a
GugaSantos <guga.santos.guga@gmail.com>
2023-04-01 06:34:25 -0300

    test: create method at acessories controller

--- src/tests/unit/controllers/acessories.controller.test.ts
+++ src/tests/unit/controllers/acessories.controller.test.ts
@@ -1,7 +1,13 @@
-import { Request } from "express";
+import { Request, Response } from "express";
 import AcessoriesController from "../../../controllers/Acessories";
 import Acessories from "../../../models/AcessoriesModel";
 import AcessoriesService from "../../../service/Acessories";
+
+import { expect } from "chai";
+import * as sinon from 'sinon';
+import { acessoryMock } from "../../mocks/acessoryMocks";
+import { carMock } from "../../mocks/carMocks";
+
 
 describe('Acessories Controller Suite Tests', () => {
   const acessoryModel = new Acessories();
@@ -11,7 +17,24 @@ describe('Acessories Controller Suite Tests', () => {
   const req = {} as Request;
   const res = {} as Response;
 
-  before(() => {})
+  before(() => {
+    sinon.stub(acessoryService, 'create').resolves(acessoryMock);
 
-  after(() => {})
+    res.status = sinon.stub().returns(res);
+    res.json = sinon.stub().returns(res);
+  })
+
+  after(() => {
+    sinon.restore();
+  })
+
+  describe('Create a new Acessory', () => {
+    it('On success', async () => {
+      req.body = carMock;
+      await acessoryController.create(req, res);
+
+      expect((res.status as sinon.SinonStub).calledWith(201)).to.be.true;
+      expect((res.json as sinon.SinonStub).calledWith(acessoryMock)).to.be.true;
+    })
+  })
 })
