42593192e9c4b9f07ae69c112242fa29b4042f34
GugaSantos <guga.santos.guga@gmail.com>
2023-04-01 10:04:29 -0300

    feat: create login controller

---
+++ src/controllers/Login.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import LoginService from '../service/Login';
+
+export default class LoginController {
+  constructor(private _service: LoginService) {}
+
+  public async Login(req: Request, res: Response) {
+    const { body } = req;
+    const user = await this._service.findOne(body.email);
+
+    return res.status(200).json(user);
+  }
+}
