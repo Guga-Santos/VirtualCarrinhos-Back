ac4ec5057e901d2c7d9a0f4e0e4552a684f5fbf7
GugaSantos <guga.santos.guga@gmail.com>
2023-04-01 10:11:45 -0300

    feat: login flow structure

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import 'express-async-errors';
 import errorHandler from './middlewares/error';
 import acessoriesRouter from './routes/Acessory';
 import carRouter from './routes/Car';
+import LoginRouter from './routes/Login';
 import userRouter from './routes/User';
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(carRouter);
 app.use(acessoriesRouter);
 app.use(userRouter);
+app.use(LoginRouter);
 app.use(errorHandler);
 
 export default app;
