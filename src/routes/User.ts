91187b80b29ac86bfa782572025190c8afc82e2e
GugaSantos <guga.santos.guga@gmail.com>
2023-04-03 09:39:23 -0300

    fix: add next at users route

--- src/routes/User.ts
+++ src/routes/User.ts
@@ -11,10 +11,10 @@ const userService = new UsersService(user);
 const userController = new UsersController(userService);
 
 route
-  .post('/users', (req, res) => userController.create(req, res))
-  .get('/users/:id', (req, res) => userController.readOne(req, res))
-  .get('/users', (req, res) => userController.read(req, res))
-  .put('/users/:id', (req, res) => userController.update(req, res))
-  .delete('/users/:id', (req, res) => userController.delete(req, res));
+  .post('/users', (req, res, next) => userController.create(req, res, next))
+  .get('/users/:id', (req, res, next) => userController.readOne(req, res, next))
+  .get('/users', (req, res, next) => userController.read(req, res, next))
+  .put('/users/:id', (req, res, next) => userController.update(req, res, next))
+  .delete('/users/:id', (req, res, next) => userController.delete(req, res, next));
 
 export default route;
