534040b5893527af9605bd2512dcb69c0162f31f
GugaSantos <guga.santos.guga@gmail.com>
2023-04-03 11:13:38 -0300

    feat: add validate auth at acessories route

--- src/routes/Acessory.ts
+++ src/routes/Acessory.ts
@@ -1,5 +1,6 @@
 /* eslint-disable sonarjs/no-duplicate-string */
 import { Router } from 'express';
+import validateJWT from '../auth/validateJWT';
 import AcessoryController from '../controllers/Acessories';
 import Acessory from '../models/AcessoriesModel';
 import AcessoryService from '../service/Acessories';
@@ -11,9 +12,10 @@ const acessoryService = new AcessoryService(acessory);
 const acessoryController = new AcessoryController(acessoryService);
 
 route
-  .post('/acessories', (req, res) => acessoryController.create(req, res))
   .get('/acessories/:id', (req, res) => acessoryController.readOne(req, res))
   .get('/acessories', (req, res) => acessoryController.read(req, res))
+  .use((req, res, next) => validateJWT(req, res, next))
+  .post('/acessories', (req, res) => acessoryController.create(req, res))
   .put('/acessories/:id', (req, res) => acessoryController.update(req, res))
   .delete('/acessories/:id', (req, res) => acessoryController.delete(req, res));
 
